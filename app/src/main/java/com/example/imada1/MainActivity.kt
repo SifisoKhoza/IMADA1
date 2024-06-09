04edf665c6c264bf5d8f64e8f35361fe7ae303b9
SifisoKhoza <168195967+SifisoKhoza@users.noreply.github.com>
2024-06-09 22:43:03 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/imada1/MainActivity.kt
+++ app/src/main/java/com/example/imada1/MainActivity.kt
@@ -9,28 +9,43 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    //global variables declaration
     private lateinit var resultTextView: TextView
     private lateinit var inputAgeEditText: EditText
     private lateinit var generateButton1: Button
     private lateinit var clearButton2: Button
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //initialisation of variables
         resultTextView = findViewById(R.id.resultTextView)
         inputAgeEditText = findViewById(R.id.inputAgeEditText)
         generateButton1 = findViewById(R.id.generateButton1)
         clearButton2 = findViewById(R.id.clearButton2)
-
+        
+        //the listener method is called to actionize the generate button
         generateButton1.setOnClickListener {
+            /*the user input element 'inputAgeEditText' is converted to
+        call 'isNullOrEmpty'*/
             if (inputAgeEditText.text.isNullOrEmpty()) {
+                //if statement condition true then user is prompted to enter age
                 inputAgeEditText.setText("Enter age")
+                
             } else {
+                /*variable is declared in else statement to
+                 convert inputAgeEditText from int to string
+                 */
                 val age: Int = inputAgeEditText.text.toString().toIntOrNull() ?: -1
+                /*value for errormessage variable is declared for the string
+                */
                 val errorMessage = "Please Enter another age between 19 and 100"
-
+                /*if statement is issued whereby the condition is if the user input age is between 19 and 100
+                    then a matching message should be displayed as string from a the declared
+                    variable 'matchingMessage'
+                 */
                 if (age in 19..100) {
+                    
                     val matchingMessage: String = when (age) {
                         95 -> """
                             Nelson Mandela (1918-2013)-Died at 95.
@@ -84,11 +99,20 @@ class MainActivity : AppCompatActivity() {
                             and played a central role in the country's
                             independence.
                         """.trimIndent()
+                        /*if the if statements conditions are not met then the
+                        else statement is called being the errormessage containing
+                        the string that was stored in it.*/
                         else -> errorMessage
                     }
+                    /*
+                    if statement conditions are if the matching message is not equal to
+                    previous else statement then a result string text will be displayed.
+                   */
                     if (matchingMessage != errorMessage) {
                         resultTextView.text = "You match with $matchingMessage"
                     } else {
+                        /*if the matching message is equal to the errormessasge
+                     then user will be prompted*/
                         resultTextView.text = errorMessage
                     }
                 } else {
@@ -96,7 +120,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
+        /*the listener method is called to clear inout stored by user when
+        clicking the clear button*/
         clearButton2.setOnClickListener {
             resultTextView.text = ""
             inputAgeEditText.text.clear()
@@ -117,7 +142,7 @@ class MainActivity : AppCompatActivity() {
     private fun enableEdgeToEdge() {
         // Implementation of this method
     }
-
+    //the validate function to validate if user input is the number between 19 and 100
     private fun validateInput(age: Int): Boolean {
         return if (age in 19..100) {
             true
